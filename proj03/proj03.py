154715f4cfad1621236b5e4878f1b655f3d8c620
Caroline Duncan <caroizzyd@gmail.com>
2017-07-11 14:38:28 -0500

    Add files via upload

--- proj03/proj03.py
+++ proj03/proj03.py
@@ -11,5 +11,23 @@ or exactly right. Keep the game going until the user types exit.
 Keep track of how many guesses the user has taken, and when the game ends, print this out.
 
 """
+x=raw_input('Guess any number between 1 and 9: ')
+x=int(x)
+import random
+variable_name=random.randint(1,9)
+while variable_name != x:
+    if x<variable_name:
+        print ('that number is too small!')
+        x=raw_input('guess again')
+        x=int(x)
+    if x > variable_name:
+        print('that number is too big!')
+        x=raw_input('guess again')
+        x=int(x)
+    if x == variable_name:
+        print ('you guessed the number!')
+
+
+
 
 
