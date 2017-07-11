c566358370d7263c6305218f29369e6e52b7f5c1
Caroline Duncan <caroizzyd@gmail.com>
2017-07-11 12:45:49 -0500

    Add files via upload

--- proj02/proj02_02.py
+++ proj02/proj02_02.py
@@ -9,12 +9,20 @@ sequence is a sequence of numbers where the next number in the sequence is the s
 previous two numbers in the sequence. The sequence looks like this: 
 1, 1, 2, 3, 5, 8, 13...
 """
-num+0
-x=raw_input('how many fibonacci number would you like to generate? ')
+num=0
+x=raw_input('how many fibonacci numbers would you like to generate? ')
 x=int(x)
-y= 1
-t=y+1
-u=t+3
+previous = 0
+current = 1
+while num < x:
+    num=num +1
+    print current
+    y=previous + current
+    previous=current
+    current=y
+
+
+
 
 
 
