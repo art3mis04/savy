6b2304b1c6a12e252236b445b70cadae8cd06ec5
Caroline Duncan <caroizzyd@gmail.com>
2017-07-12 11:23:20 -0500

    Add files via upload

--- proj03/proj03.py
+++ proj03/proj03.py
@@ -11,23 +11,35 @@ or exactly right. Keep the game going until the user types exit.
 Keep track of how many guesses the user has taken, and when the game ends, print this out.
 
 """
-x=raw_input('Guess any number between 1 and 9: ')
-x=int(x)
-import random
-variable_name=random.randint(1,9)
-while variable_name != x:
-    if x<variable_name:
-        print ('that number is too small!')
-        x=raw_input('guess again')
-        x=int(x)
-    if x > variable_name:
-        print('that number is too big!')
-        x=raw_input('guess again')
-        x=int(x)
-    if x == variable_name:
-        print ('you guessed the number!')
-
-
-
-
-
+loop_var = True
+while loop_var == True:
+    counter = 0
+    a = raw_input("Enter a number one through fifteen. ")
+    a = int(a)
+    counter = counter + 1
+    import random
+    random_num = random.randint(1,15)
+    while random_num != a and counter < 5:
+        if random_num < a:
+            print ('That number is too high. ')
+            a = raw_input('Guess again. ')
+            a = int(a)
+            counter = counter + 1
+        if random_num > a:
+            print('That number is too low. ')
+            a = raw_input('guess again ')
+            a = int(a)
+            counter = counter + 1
+        if random_num == a:
+            print ('You guessed the number! ')
+            print ('You ran out of guesses!')
+            b = raw_input('Would you like to play again? Type yes or no. ')
+            b = str(b)
+            if b == 'no':
+                loop_var = False
+        if counter == 5:
+            print ('You ran out of guesses!')
+            b= raw_input ('Would you like to play again? Type yes or no. ')
+            b=str(b)
+            if b == 'no':
+                loop_var = False
